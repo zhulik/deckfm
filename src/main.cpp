a3804bd1f240400fcf25aa9cd028dcf63a446497
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2022-10-11 00:02:19 +0200

    Add SteamAPI

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 
 #include "application.h"
 
+#include "steam/steam_api.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    if (!SteamAPI_Init()) {
+        qWarning() << "SteamAPI cannot be initialized";
+    }
     Application app(argc, argv);
 
     return app.exec();
