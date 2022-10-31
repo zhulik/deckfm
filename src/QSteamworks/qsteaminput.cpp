342d60d491c5faa75125f9a0ff101775a31c9540
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2022-10-31 22:10:05 +0100

    Fix

--- src/QSteamworks/qsteaminput.cpp
+++ src/QSteamworks/qsteaminput.cpp
@@ -451,5 +451,5 @@ void QSteamInput::setActionSetLayer(const QString &newActionSetLayer) {
       return;
     }
   }
-  throw std::runtime_error(QString("Cannot find action set layer %1").arg(newActionSetLayer).toLocal8Bit());
+  //  throw std::runtime_error(QString("Cannot find action set layer %1").arg(newActionSetLayer).toLocal8Bit());
 }
