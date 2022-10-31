04a5dbfc4423689f921912b90069438dc7e9dc0b
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2022-11-01 00:07:57 +0100

    Debug

--- src/QSteamworks/qsteaminput.cpp
+++ src/QSteamworks/qsteaminput.cpp
@@ -446,14 +446,14 @@ const QSteamworks::ActionSetLayer &QSteamInput::currentActionSetLayer() const { 
 const QString &QSteamInput::qmlActionSetLayer() const { return m_currentActionSetLayer.name(); }
 
 void QSteamInput::setActionSetLayer(const QString &newActionSetLayer) {
+  if (m_actionSet.name() == "" || newActionSetLayer == "" || m_currentActionSetLayer.name() == newActionSetLayer)
+    return;
+
   m_currentActionSetLayer = ActionSetLayer(
       -1, QString("%1 %2").arg(m_currentActionSetLayer.name() == newActionSetLayer).arg(newActionSetLayer == ""),
       QList<Action>());
   emit actionSetLayerChanged();
   return;
-
-  if (m_actionSet.name() == "" || newActionSetLayer == "" || m_currentActionSetLayer.name() == newActionSetLayer)
-    return;
 
   if (!m_iga.qmlActionSetLayers().contains(newActionSetLayer)) {
     throw std::runtime_error(QString("Cannot find action set layer %1").arg(newActionSetLayer).toLocal8Bit());
