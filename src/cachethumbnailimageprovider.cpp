645ee63247067bf5b3733db6dd96589e26f4eb64
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-07-13 18:07:51 +0200

    Refactoring

--- src/cachethumbnailimageprovider.cpp
+++ src/cachethumbnailimageprovider.cpp
@@ -5,50 +5,43 @@
 #include <QtConcurrent/QtConcurrent>
 
 #include "abstractcache.h"
+#include "qfilesystemwatcher.h"
+
+static const auto ThumbnailSizebase = 125;
 
 class AsyncImageResponse : public QQuickImageResponse {
 public:
-  AsyncImageResponse(const QString &id, const QSize &requestedSize, AbstractCache *cache) {
-    auto watcher = new QFutureWatcher<QImage>();
+  AsyncImageResponse(const QString &id, const QSize &requestedSize, AbstractCache *cache)
+      : m_id(id), m_requestedSize(requestedSize) {
+    auto watcher = new QFutureWatcher<void>(this);
+    connect(watcher, &QFutureWatcher<void>::finished, this, &AsyncImageResponse::finished);
+    connect(watcher, &QFutureWatcher<void>::finished, watcher, &QFileSystemWatcher::deleteLater);
 
     auto f = QtConcurrent::run([id, requestedSize, this, cache]() {
-      QImage result;
-
-      if (requestedSize.isEmpty()) {
-        return result;
+      if (requestedSize.isEmpty() || cache == nullptr) {
+        m_image = fetchImage().first;
+        return;
       }
 
-      if (cache != nullptr) {
-        auto data = cache->withCache(imageCacheId(id, requestedSize), [id, requestedSize]() {
-          QByteArray ba;
-          QImage image(id); // TODO: add support for network sources
-          if (image.isNull()) {
-            return ba;
-          }
-          if (!image.isNull()) {
-            image = image.scaled(requestedSize, Qt::KeepAspectRatio);
-          }
+      auto data = cache->withCache(imageCacheId(m_id, requestedSize), [this]() {
+        auto pair = fetchImage();
+        m_image = pair.first;
+        auto skipCache = pair.second;
+
+        QByteArray ba;
+
+        if (!m_image.isNull() && !skipCache) {
           QBuffer buffer(&ba);
           buffer.open(QIODevice::WriteOnly);
-          Q_ASSERT(image.save(&buffer, "png"));
-          qDebug() << ba.size();
-          return ba;
-        });
-
-        result.loadFromData(data);
-      } else {
-        QImage image(id); // TODO: add support for network sources
-        if (!image.isNull()) {
-          result = image.scaled(requestedSize, Qt::KeepAspectRatio);
+          Q_ASSERT(m_image.save(&buffer, "png"));
         }
-      }
-      return result;
-    });
 
-    connect(watcher, &QFutureWatcher<QImage>::finished, watcher, [this, f, watcher]() {
-      m_image = f.result();
-      emit finished();
-      watcher->deleteLater();
+        return ba;
+      });
+
+      if (!data.isEmpty() && !data.isNull()) {
+        Q_ASSERT(m_image.loadFromData(data));
+      }
     });
 
     watcher->setFuture(f);
@@ -59,10 +52,22 @@ public:
   }
 
 private:
+  QString m_id;
+  QSize m_requestedSize;
   QImage m_image;
 
   QString imageCacheId(const QString &id, const QSize &size) const {
     return QString("%1//%2x%3").arg(id).arg(size.width()).arg(size.height());
+  }
+
+  QPair<QImage, bool> fetchImage() const {
+    QImage image(m_id); // TODO: add support for network sources
+    auto skipCache = false;
+
+    if (!image.isNull() && !m_requestedSize.isNull()) {
+      image = image.scaled(m_requestedSize, Qt::KeepAspectRatio);
+    }
+    return QPair{image, skipCache};
   }
 };
 
